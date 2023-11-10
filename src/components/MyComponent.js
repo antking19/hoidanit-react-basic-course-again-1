89a36796edc6672d33ba301ba92ac54edbe9564d
antking19 <cuong.truongkien@gmail.com>
2023-11-10 10:28:21 +0700

    video 21 Form with React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,11 +5,35 @@ class MyComponent extends React.Component {
         name: "Marcus",
         age: 28,
     };
+
+    handleClick() {
+        console.log(">> Click Event");
+        this.setState({
+            name: "Kien Cuong",
+            age: 33,
+        });
+    }
+
+    handleOnChangeInput = (event) => {
+        event.preventDefault();
+        this.setState({
+            name: event.target.value,
+        });
+    };
+
     render() {
         return (
             <div>
                 My first component <br />
                 My name is {this.state.name}, I'm {this.state.age}
+                {/* <button onClick={() => this.handleClick()}>Click Me</button> */}
+                <form>
+                    <input
+                        type="text"
+                        onChange={(event) => this.handleOnChangeInput(event)}
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
